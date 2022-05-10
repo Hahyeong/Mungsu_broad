06bda8063c09d7712471fa60b094966575ae7c53
hannah0125 <jhn0125@ewhain.net>
2022-05-10 13:44:06 +0900

    AUDIO, CAMERA 권한 승인 / PreviewHolder 수정 필요

--- app/src/main/java/com/ewha/mungsubroad/MainActivity.java
+++ app/src/main/java/com/ewha/mungsubroad/MainActivity.java
@@ -16,16 +16,23 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+import android.se.omapi.Session;
 
 import com.amazonaws.ivs.broadcast.BroadcastException;
 import com.amazonaws.ivs.broadcast.BroadcastSession;
+import com.amazonaws.ivs.broadcast.Device;
+import com.amazonaws.ivs.broadcast.ImageDevice;
+import com.amazonaws.ivs.broadcast.ImagePreviewView;
 import com.amazonaws.ivs.broadcast.Presets;
 
 public class MainActivity extends AppCompatActivity {
-    Button broadbutton;
+    Button startbroad;
+    Button stopbroad;
+    LinearLayout previewHolder;
 
-    // eventlistener -> 상태 업데이트, 오류 및 세션 변경
+    // Event Listener
     BroadcastSession.Listener broadcastListener =
             new BroadcastSession.Listener() {
                 @Override
@@ -39,28 +46,69 @@ public class MainActivity extends AppCompatActivity {
                 }
             };
 
+    // getApplicationContext() <-- null error 고치기
     Context ctx = getApplicationContext();
-    BroadcastSession = new BroadcastSession(ctx, broadcastListener, Presets.Configuration.STANDARD_PORTRAIT, Presets.Devices.FRONT_CAMERA(ctx));
+    BroadcastSession broadcastSession = new BroadcastSession(ctx,
+            broadcastListener,
+            Presets.Configuration.STANDARD_PORTRAIT,
+            Presets.Devices.FRONT_CAMERA(ctx));
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        broadbutton = (Button)findViewById(R.id.startbroad);
+        startbroad = (Button)findViewById(R.id.startbroad);
+        stopbroad = (Button)findViewById(R.id.stopbroad);
 
-        broadbutton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA);
-                if(permissionCheck == PackageManager.PERMISSION_DENIED) {
-                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CAMERA},0);
-                } else {
-                    // 권한 승인받았을 때
-                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(intent,1);
+        // 오디오 및 카메라 권한 설정
+        final String[] requiredPermissions =
+                { Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO };
+
+        for (String permission : requiredPermissions) {
+            if (ContextCompat.checkSelfPermission(this, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // If any permissions are missing we want to just request them all.
+                ActivityCompat.requestPermissions(this, requiredPermissions, 0x100);
+                break;
+            }
+        }
+
+        // 녹화 시작
+        startbroad.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               broadcastSession.start("rtmps://3c90cab980bd.global-contribute.live-video.net:443/app/", "sk_ap-northeast-2_yFcoiZCkIOLg_vKCoVZPK8UBYWahI8dUXOVUmQlwASj");
+           }
+        });
+
+        broadcastSession.awaitDeviceChanges(() -> {
+            for(Device device: broadcastSession.listAttachedDevices()) {
+                // Find the camera we attached earlier
+                if(device.getDescriptor().type == Device.Descriptor.DeviceType.CAMERA) {
+                    LinearLayout previewHolder = findViewById(R.id.previewHolder);
+                    ImagePreviewView preview = ((ImageDevice)device).getPreviewView();
+                    preview.setLayoutParams(new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.MATCH_PARENT));
+                    previewHolder.addView(preview);
                 }
             }
         });
 
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode,
+                permissions, grantResults);
+        if (requestCode == 0x100) {
+            for (int result : grantResults) {
+                if (result == PackageManager.PERMISSION_DENIED) {
+                    return;
+                }
+            }
+        }
     }
 }
